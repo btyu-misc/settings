ea036687246e53e65a7b9ab9af7073621bde0ef5
Benjamin Yu <123601428+btyu-misc@users.noreply.github.com>
2025-02-16 11:44:36 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+function general() {
+  document.getElementById("general").scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+
+}
+
+function security() {
+  document.getElementById("security").scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+
+}
+
+function subscriptions() {
+  document.getElementById("subscriptions").scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+
+}
+
+function cancelm() {
+  document.getElementById("cancel").scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+
+}
+
+function policy() {
+  document.getElementById("policy").scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+
+}
+
+function password() {
+  alert("We can't change your password quite yet, but stay tuned!");
+}
+
+function subscription() {
+  alert("We can't change your subscription quite yet, but stay tuned!");
+}
+
+function cancel() {
+  alert("We can't cancel your subscription, but stay tuned!");
+}
